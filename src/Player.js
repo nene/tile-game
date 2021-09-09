e9894b43f700814372fb47983b1ba2aaf92bcf85
Rene Saarsoo <nene@triin.net>
2021-09-09 17:50:14 +0300

    Store speed as vector

--- src/Player.js
+++ src/Player.js
@@ -3,7 +3,7 @@ import { SpriteSheet } from "./SpriteSheet";
 export class Player {
   constructor(images) {
     this.coord = [0, 0];
-    this.speed = 0;
+    this.speed = [0, 0];
 
     this.standRight = new SpriteSheet(images.get("walkRight"), [32, 32], 1);
     this.standLeft = new SpriteSheet(images.get("walkLeft"), [32, 32], 1);
@@ -15,27 +15,27 @@ export class Player {
   }
 
   moveRight() {
-    this.speed = 3;
+    this.speed = [3, 0];
     this.activeSpriteSheet = this.walkRight;
   }
 
   moveLeft() {
-    this.speed = -3;
+    this.speed = [-3, 0];
     this.activeSpriteSheet = this.walkLeft;
   }
 
   stop() {
-    if (this.speed > 0) {
+    if (this.speed[0] > 0) {
       this.activeSpriteSheet = this.standRight;
     }
-    if (this.speed < 0) {
+    if (this.speed[0] < 0) {
       this.activeSpriteSheet = this.standLeft;
     }
-    this.speed = 0;
+    this.speed = [0, 0];
   }
 
   tick(screen) {
-    this.coord = [this.coord[0] + this.speed, this.coord[1]];
+    this.coord = [this.coord[0] + this.speed[0], this.coord[1]];
     if (this.coord[0] > screen.width() - 32) {
       this.coord = [screen.width() - 32, this.coord[1]];
     }
