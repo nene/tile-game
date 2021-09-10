50a78985cdf78849e382153cab9c4564beea0bee
Rene Saarsoo <nene@triin.net>
2021-09-10 16:43:47 +0300

    Add more 3d grass sprite

--- src/game/ImageLibrary.ts
+++ src/game/ImageLibrary.ts
@@ -4,6 +4,7 @@ import grass1 from "./sprites/grass1.png";
 import grass2 from "./sprites/grass2.png";
 import grass3 from "./sprites/grass3.png";
 import grass4 from "./sprites/grass4.png";
+import grass13d from "./sprites/grass1-3d.png";
 import walkLeft from "./sprites/walk-left.png";
 import walkRight from "./sprites/walk-right.png";
 import walkForward from "./sprites/walk-forward.png";
@@ -19,6 +20,7 @@ export class ImageLibrary {
     this.images.grass2 = await loadImage(grass2);
     this.images.grass3 = await loadImage(grass3);
     this.images.grass4 = await loadImage(grass4);
+    this.images.grass13d = await loadImage(grass13d);
 
     this.images.walkLeft = await loadImage(walkLeft);
     this.images.walkRight = await loadImage(walkRight);
