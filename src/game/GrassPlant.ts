a4807117bd495898edca1971510210efc38ef092
Rene Saarsoo <nene@triin.net>
2021-09-13 13:46:51 +0300

    Use noise to decide plant types

--- src/game/GrassPlant.ts
+++ src/game/GrassPlant.ts
@@ -5,13 +5,15 @@ import { GameObject } from "./GameObject";
 import { SpriteAnimation } from "./SpriteAnimation";
 import { SpriteSheet } from "./SpriteSheet";
 
+export type GrassPlantType = 1 | 2 | 3 | 4;
+
 export class GrassPlant implements GameObject {
   private coord: Coord;
   private animation: SpriteAnimation;
   private offset: Coord = [-8, -16];
 
-  constructor(images: ImageLibrary, coord: Coord) {
-    const img = images.get("grass" + (rand(4) + 1));
+  constructor(images: ImageLibrary, grassType: GrassPlantType, coord: Coord) {
+    const img = images.get("grass" + grassType);
     this.animation = new SpriteAnimation(
       new SpriteSheet(img, [32, 32], [1, 6]),
       { ticksPerFrame: 2, currentFrame: rand(6) }
--- src/game/generatePlants.ts
+++ src/game/generatePlants.ts
@@ -1,14 +1,32 @@
 import SimplexNoise from "simplex-noise";
 import { GameGrid } from "./GameGrid";
-import { GrassPlant } from "./GrassPlant";
+import { GrassPlant, GrassPlantType } from "./GrassPlant";
 import { ImageLibrary } from "./ImageLibrary";
 import { SurfaceMap, SurfaceType } from "./SurfaceMap";
 
 export function generatePlants(grid: GameGrid, surface: SurfaceMap, images: ImageLibrary): GrassPlant[] {
-  const noise = new SimplexNoise();
+  const plantExistanceNoise = new SimplexNoise();
+  const plantTypeNoise = new SimplexNoise();
 
   return grid.allTiles()
     .filter(([x, y]) => surface[x][y] === SurfaceType.grass)
-    .filter(([x, y]) => noise.noise2D(x / 10, y / 10) > 0.5)
-    .map((colRow) => new GrassPlant(images, grid.tileToScreenCoord(colRow)));
+    .filter(([x, y]) => plantExistanceNoise.noise2D(x / 10, y / 10) > 0.5)
+    .map(([x, y]) => new GrassPlant(
+      images,
+      pickGrassPlantType(plantTypeNoise.noise2D(x / 10, y / 10)),
+      grid.tileToScreenCoord([x, y]),
+    ));
+}
+
+function pickGrassPlantType(x: number): GrassPlantType {
+  if (x > 0.5) {
+    return 1;
+  }
+  if (x > 0) {
+    return 2;
+  }
+  if (x > -0.5) {
+    return 3;
+  }
+  return 4;
 }
