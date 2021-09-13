e7c5b411cc3bd18c8368585ebf70c8345980a10e
Rene Saarsoo <nene@triin.net>
2021-09-13 13:34:03 +0300

    Generate clusters of plants using noise

--- src/game/generateGrass.ts
+++ src/game/generateGrass.ts
@@ -1,11 +1,14 @@
-import { shuffle, take } from "lodash";
+import SimplexNoise from "simplex-noise";
 import { GameGrid } from "./GameGrid";
 import { Grass } from "./Grass";
 import { ImageLibrary } from "./ImageLibrary";
 import { SurfaceMap, SurfaceType } from "./SurfaceMap";
 
 export function generateGrass(grid: GameGrid, surface: SurfaceMap, images: ImageLibrary): Grass[] {
-  return take(shuffle(grid.allTiles()), 50)
+  const noise = new SimplexNoise();
+
+  return grid.allTiles()
     .filter(([x, y]) => surface[x][y] === SurfaceType.grass)
+    .filter(([x, y]) => noise.noise2D(x / 10, y / 10) > 0.5)
     .map((colRow) => new Grass(images, grid.tileToScreenCoord(colRow)));
 }
