0202457197258cd4fdf949bcbec2275f64731949
Rene Saarsoo <nene@triin.net>
2021-09-18 14:02:18 +0300

    Extract PlainBackground class

--- src/game/Background.ts
+++ src/game/Background.ts
@@ -2,17 +2,17 @@ import { Coord, coordEq } from "./Coord";
 import { GameGrid } from "./GameGrid";
 import { Ground } from "./Ground";
 import { PixelScreen } from "./PixelScreen";
+import { PlainBackground } from "./PlainBackground";
 import { SpriteLibrary } from "./SpriteLibrary";
-import { SpriteSheet } from "./SpriteSheet";
 import { SurfaceMap } from "./SurfaceMap";
 
 export class Background {
-  private bgSprites: SpriteSheet;
+  private plainBg: PlainBackground;
   private ground: Ground;
   private previousOffset: Coord = [-1, -1];
 
-  constructor(private grid: GameGrid, surface: SurfaceMap, sprites: SpriteLibrary) {
-    this.bgSprites = sprites.get('grass-bg');
+  constructor(grid: GameGrid, surface: SurfaceMap, sprites: SpriteLibrary) {
+    this.plainBg = new PlainBackground(grid, sprites);
     this.ground = new Ground(grid, surface, sprites);
   }
 
@@ -22,18 +22,9 @@ export class Background {
       return;
     }
 
-    this.grid.forEachTile(([x, y]) => {
-      if (isEven(x) && isEven(y)) {
-        screen.drawSprite(this.bgSprites.getSprite([0, 0]), this.grid.tileToScreenCoord([x, y]));
-      }
-    });
-
+    this.plainBg.paint(screen);
     this.ground.paint(screen);
     screen.saveBg();
     this.previousOffset = screen.getOffset();
   }
-}
-
-function isEven(x: number): boolean {
-  return x % 2 === 0;
 }
