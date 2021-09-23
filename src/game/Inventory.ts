d472550df7130c70cc64fcd576e4a41c2a86c6c0
Rene Saarsoo <nene@triin.net>
2021-09-23 16:33:02 +0300

    Remove events support from Inventory

--- src/game/Inventory.ts
+++ src/game/Inventory.ts
@@ -1,12 +1,9 @@
 import { Coord } from "./Coord";
 import { GameItem } from "./items/GameItem";
 
-type OnChangeCallback = (items: GameItem[]) => void;
-
 export class Inventory {
   private _items: GameItem[];
   private _size: Coord;
-  private _callbacks: OnChangeCallback[] = [];
 
   constructor(cfg: { size: Coord, items?: GameItem[] }) {
     this._size = cfg.size;
@@ -16,13 +13,11 @@ export class Inventory {
   add(item: GameItem) {
     if (this._items.length < this.maxItems()) {
       this._items = [...this._items, item];
-      this.notify();
     }
   }
 
   remove(item: GameItem) {
     this._items = this._items.filter((it) => it !== item);
-    this.notify();
   }
 
   items(): GameItem[] {
@@ -31,13 +26,5 @@ export class Inventory {
 
   maxItems(): number {
     return this._size[0] * this._size[1];
-  }
-
-  onChange(callback: OnChangeCallback) {
-    this._callbacks.push(callback);
-  }
-
-  private notify() {
-    this._callbacks.forEach((cb) => cb(this._items));
   }
 }
