464de97419650a78237e8d127da81c94be397fed
Rene Saarsoo <nene@triin.net>
2021-09-24 15:13:22 +0300

    New drinking sprites

--- src/game/SpriteLibrary.ts
+++ src/game/SpriteLibrary.ts
@@ -1,6 +1,8 @@
 import cfeBg from "./sprites/cfe-bg.png";
 import cfeReb from "./sprites/cfe-reb.png";
 import cfeKsv from "./sprites/cfe-ksv.png";
+import cfeKsvDrinking from "./sprites/cfe-ksv-drinking.png";
+import cfeKsvHand from "./sprites/cfe-ksv-hand.png";
 import callout from "./sprites/callout.png";
 import table from "./sprites/table.png";
 import fridge from "./sprites/fridge.png";
@@ -8,6 +10,7 @@ import beerCabinet from "./sprites/beer-cabinet.png";
 import slot from "./sprites/slot.png";
 import cursor from "./sprites/cursor.png";
 import beerLg from "./sprites/beer-lg.png";
+import beerSm from "./sprites/beer-sm.png";
 import bottle from "./sprites/bottle.png";
 import { Coord } from "./Coord";
 import { SpriteSheet } from "./SpriteSheet";
@@ -29,6 +32,8 @@ export class SpriteLibrary {
       'cfe-reb': new SpriteSheet(await loadImage(cfeReb), { size: PLAYER_SIZE, colsRows: [4, 4], offset: PLAYER_OFFSET }),
 
       'cfe-ksv': new SpriteSheet(await loadImage(cfeKsv), { size: PLAYER_SIZE, colsRows: [3, 1], offset: PLAYER_OFFSET }),
+      'cfe-ksv-drinking': new SpriteSheet(await loadImage(cfeKsvDrinking), { size: PLAYER_SIZE, colsRows: [1, 1], offset: PLAYER_OFFSET }),
+      'cfe-ksv-hand': new SpriteSheet(await loadImage(cfeKsvHand), { size: [7, 6], colsRows: [1, 1] }),
       'callout': new SpriteSheet(await loadImage(callout), { size: [26, 14], colsRows: [1, 1], offset: [-10, -13] }),
 
       'table': new SpriteSheet(await loadImage(table), { size: [64, 22], colsRows: [1, 1], offset: [0, -6] }),
@@ -39,6 +44,7 @@ export class SpriteLibrary {
       'cursor': new SpriteSheet(await loadImage(cursor), { size: [16, 16], colsRows: [1, 1] }),
 
       'beer-lg': new SpriteSheet(await loadImage(beerLg), { size: [16, 16], colsRows: [5, 1] }),
+      'beer-sm': new SpriteSheet(await loadImage(beerSm), { size: [10, 10], colsRows: [5, 1] }),
       'bottle': new SpriteSheet(await loadImage(bottle), { size: [16, 16], colsRows: [2, 3] }),
     };
   }
