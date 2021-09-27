fac9ea0fb827fa32ce32af8a947cfe801c4a9607
Rene Saarsoo <nene@triin.net>
2021-09-27 18:53:10 +0300

    Extracy InventoryController from UiController

--- src/game/UiController.ts
+++ src/game/UiController.ts
@@ -1,67 +1,37 @@
 import { Inventory } from "./Inventory";
 import { PixelScreen } from "./PixelScreen";
-import { SpriteLibrary } from "./SpriteLibrary";
-import { InventoryView } from "./InventoryView";
-import { Coord, coordAdd, coordSub } from "./Coord";
+import { Coord } from "./Coord";
 import { GameItem } from "./items/GameItem";
-import { Sprite } from "./Sprite";
-import { debounce } from "lodash";
+import { InventoryController } from "./InventoryController";
 import { Dialog } from "./Dialog";
 
 export class UiController {
-  private playerInventoryView: InventoryView;
-  private objectInventory?: Inventory;
-  private objectInventoryView?: InventoryView;
-  private mouseCoord: Coord = [-16, -16];
-  private selectedItem?: GameItem;
-  private hoveredItem?: GameItem;
-  private cursor: Sprite;
+  private inventoryController: InventoryController;
   private dialog?: Dialog;
 
   constructor(private playerInventory: Inventory) {
-    this.playerInventoryView = new InventoryView(playerInventory, [107, 200 - 22]);
-    this.cursor = SpriteLibrary.get("cursor").getSprite([0, 0]);
+    this.inventoryController = new InventoryController(playerInventory);
   }
 
   getSelectedItem(): GameItem | undefined {
-    return this.selectedItem;
+    return this.inventoryController.getSelectedItem();
   }
 
   removeSelectedItem() {
-    this.selectedItem = undefined;
+    this.inventoryController.removeSelectedItem();
   }
 
   showInventory(inventory: Inventory, title?: string) {
-    this.objectInventory = inventory;
-    this.objectInventoryView = new InventoryView(inventory, [130, 50], title);
-  }
-
-  hideInventory() {
-    this.objectInventory = undefined;
-    this.objectInventoryView = undefined;
+    this.inventoryController.showInventory(inventory, title);
   }
 
   paint(screen: PixelScreen) {
-    if (this.objectInventoryView) {
-      this.drawOverlay(screen);
-      this.objectInventoryView.paint(screen);
-    }
-
     if (this.dialog) {
       this.drawOverlay(screen);
       this.dialog.paint(screen);
     } else {
-      this.playerInventoryView.paint(screen);
+      this.inventoryController.paint(screen);
     }
-
-    if (this.selectedItem) {
-      screen.drawSprite(this.selectedItem.getSprite(), coordSub(this.mouseCoord, [8, 8]), { fixed: true });
-    }
-    if (this.hoveredItem) {
-      screen.drawText(this.hoveredItem.getName(), "#3e2821", "#c8b997", coordAdd(this.mouseCoord, [10, 10]));
-    }
-
-    screen.drawSprite(this.cursor, this.mouseCoord, { fixed: true });
   }
 
   private drawOverlay(screen: PixelScreen) {
@@ -69,91 +39,17 @@ export class UiController {
   }
 
   handleClick(screenCoord: Coord): boolean {
-    this.hoveredItem = undefined;
-
     if (this.dialog) {
       if (this.dialog.isCoordInView(screenCoord)) {
         this.dialog = undefined; // Close the dialog
       }
       return true;
     }
-    else if (this.playerInventoryView.isCoordInView(screenCoord)) {
-      this.handleInventoryClick(screenCoord, this.playerInventory, this.playerInventoryView);
-      return true;
-    }
-    else if (this.objectInventory && this.objectInventoryView) {
-      if (this.objectInventoryView.isCoordInView(screenCoord)) {
-        this.handleInventoryClick(screenCoord, this.objectInventory, this.objectInventoryView);
-      } else {
-        this.hideInventory();
-      }
-      return true;
-    }
-    return false;
+    return this.inventoryController.handleClick(screenCoord);
   }
 
   handleHover(screenCoord: Coord): boolean {
-    this.mouseCoord = screenCoord;
-    this.hoveredItem = undefined;
-    this.showTooltipAfterDelay(screenCoord);
-    return true;
-  }
-
-  private showTooltipAfterDelay = debounce((screenCoord: Coord) => {
-    this.hoveredItem = this.getHoveredInventoryItem(screenCoord);
-  }, 500);
-
-  private getHoveredInventoryItem(coord: Coord): GameItem | undefined {
-    const item = this.getInventoryItemAtCoord(coord, this.playerInventory, this.playerInventoryView);
-    if (item) {
-      return item;
-    }
-    if (this.objectInventory && this.objectInventoryView) {
-      return this.getInventoryItemAtCoord(coord, this.objectInventory, this.objectInventoryView);
-    }
-    return undefined;
-  }
-
-  private getInventoryItemAtCoord(coord: Coord, inventory: Inventory, inventoryView: InventoryView): GameItem | undefined {
-    if (inventoryView.isCoordInView(coord)) {
-      const slot = inventoryView.getSlotAtCoord(coord);
-      return slot && inventory.itemAt(slot);
-    }
-    return undefined;
-  }
-
-  private handleInventoryClick(coord: Coord, inventory: Inventory, inventoryView: InventoryView) {
-    const slotCoord = inventoryView.getSlotAtCoord(coord);
-    if (!slotCoord) {
-      return; // no slot clicked
-    }
-
-    const item = inventory.itemAt(slotCoord);
-    if (item && !this.selectedItem) {
-      // Take item from inventory
-      inventory.remove(item);
-      this.selectedItem = item;
-    }
-    else if (!item && this.selectedItem && !inventory.isFull()) {
-      // Place item at hand to inventory
-      inventory.addAt(slotCoord, this.selectedItem);
-      this.selectedItem = undefined;
-    }
-    else if (item && this.selectedItem) {
-      // Combine these items if possible
-      const combinedItems = item.combine(this.selectedItem);
-      if (combinedItems.length > 0) {
-        inventory.remove(item);
-        inventory.addAt(slotCoord, combinedItems[0]);
-        this.selectedItem = combinedItems[1]; // possibly nothing
-      }
-      else {
-        // Otherwise swap item at hand with item in inventory
-        inventory.remove(item);
-        inventory.addAt(slotCoord, this.selectedItem);
-        this.selectedItem = item;
-      }
-    }
+    return this.inventoryController.handleHover(screenCoord);
   }
 
   showDialog(dialog: Dialog) {
