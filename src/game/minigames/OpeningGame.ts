7da389f5297b0b9b8fe66b39b93fcd6d534cb0d0
Rene Saarsoo <nene@triin.net>
2021-09-27 20:22:21 +0300

    Animate the moving bottle

--- src/game/minigames/OpeningGame.ts
+++ src/game/minigames/OpeningGame.ts
@@ -1,4 +1,4 @@
-import { Coord, coordDistance, coordDiv, tileToScreenCoord } from "../Coord";
+import { Coord, coordAdd, coordDistance, coordDiv, coordMul, tileToScreenCoord } from "../Coord";
 import { BeerBottle } from "../items/BeerBottle";
 import { BottleOpener } from "../items/BottleOpener";
 import { PixelScreen } from "../PixelScreen";
@@ -7,11 +7,15 @@ import { Sprite } from "../Sprite";
 import { SpriteLibrary } from "../SpriteLibrary";
 import { SpriteSheet } from "../SpriteSheet";
 import { MiniGame } from "./MiniGame";
+import SimplexNoise from "simplex-noise";
 
 enum CaptureStatus {
   miss = 0,
   hit = 1,
 }
+
+const NOISE_SCALE = 100;
+const BOUNDING_BOX: Coord = [100, 100];
 
 export class OpeningGame implements MiniGame {
   private bgSprite: Sprite;
@@ -21,7 +25,9 @@ export class OpeningGame implements MiniGame {
   private bottleCoord: Coord;
   private openerCoord: Coord;
   private captureStatus = CaptureStatus.miss;
-  private captureThreshold = 2;
+  private captureThreshold = 5;
+  private noise: SimplexNoise;
+  private counter = 0;
 
   constructor(private bottle: BeerBottle, private opener: BottleOpener) {
     this.bgSprite = SpriteLibrary.get("opening-game-bg").getSprite([0, 0]);
@@ -30,10 +36,18 @@ export class OpeningGame implements MiniGame {
     this.openerSprite = SpriteLibrary.get("bottle-opener-xl").getSprite([0, 0]);
     this.bottleCoord = [100, 100];
     this.openerCoord = [120, 120];
+    this.noise = new SimplexNoise();
   }
 
   tick() {
+    this.bottleCoord = coordAdd([100, 100], this.nextNoiseCoord());
+  }
 
+  private nextNoiseCoord(): Coord {
+    this.counter++;
+    const x = this.noise.noise2D(this.counter / NOISE_SCALE, 1);
+    const y = this.noise.noise2D(1, this.counter / NOISE_SCALE);
+    return coordMul([x, y], BOUNDING_BOX).map(Math.floor) as Coord;
   }
 
   paint(screen: PixelScreen) {
