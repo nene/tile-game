37efd218bb8dd4ed5ab0ccf41e239289b55cd701
Rene Saarsoo <nene@triin.net>
2021-10-01 12:40:55 +0300

    Extract tilesCoveredBy()

--- src/game/groupByTiles.ts
+++ src/game/groupByTiles.ts
@@ -7,20 +7,28 @@ type GroupMap<T> = Record<string, T[]>;
 export function groupByTiles<T extends ObjectWithBounds>(objects: T[]): GroupMap<T> {
   const tileMap: GroupMap<T> = {};
   objects.forEach((obj) => {
-    const { coord: offset, size } = obj.boundingBox();
-    const coord = coordAdd(obj.getCoord(), offset);
-    const topLeftTile = screenToTileCoord(coord);
-    const bottomRightTile = screenToFractionalTileCoord(coordAdd(coord, size)).map(Math.ceil) as Coord;
-
-    for (let dx = topLeftTile[0]; dx < bottomRightTile[0]; dx++) {
-      for (let dy = topLeftTile[1]; dy < bottomRightTile[1]; dy++) {
-        const key = [dx, dy].join(",");
-        const array = tileMap[key] ?? [];
-        array.push(obj);
-        tileMap[key] = array;
-      }
-    }
+    tilesCoveredBy(obj).forEach((tileCoord) => {
+      const key = tileCoord.join(",");
+      const array = tileMap[key] ?? [];
+      array.push(obj);
+      tileMap[key] = array;
+    });
   });
 
   return tileMap;
+}
+
+export function tilesCoveredBy<T extends ObjectWithBounds>(obj: T): Coord[] {
+  const { coord: offset, size } = obj.boundingBox();
+  const coord = coordAdd(obj.getCoord(), offset);
+  const topLeftTile = screenToTileCoord(coord);
+  const bottomRightTile = screenToFractionalTileCoord(coordAdd(coord, size)).map(Math.ceil) as Coord;
+
+  const tiles: Coord[] = [];
+  for (let x = topLeftTile[0]; x < bottomRightTile[0]; x++) {
+    for (let y = topLeftTile[1]; y < bottomRightTile[1]; y++) {
+      tiles.push([x, y]);
+    }
+  }
+  return tiles;
 }
