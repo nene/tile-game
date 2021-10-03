dd9c5852a8ca735d1bd4bf0e4f2ec00246b7e298
Rene Saarsoo <nene@triin.net>
2021-10-03 17:49:14 +0300

    Extract drawItem() method

--- src/game/shop/ShopListView.ts
+++ src/game/shop/ShopListView.ts
@@ -26,20 +26,23 @@ export class ShopListView {
 
   paint(screen: PixelScreen) {
     const itemRect: Rect = { coord: this.rect.coord, size: [this.rect.size[0] - 7, 20] };
-    const iconRect: Rect = { coord: coordAdd(this.rect.coord, [2, 2]), size: [16, 16] };
-    const nameCoord: Coord = coordAdd(iconRect.coord, [18, -1]);
-    const descriptionCoord: Coord = coordAdd(iconRect.coord, [18, 9]);
-    const goldCoord: Coord = coordAdd(itemRect.coord, [itemRect.size[0] - 10, 2]);
-    const priceCoord: Coord = coordAdd(goldCoord, [-2, -1]);
+
     this.items.forEach((item, i) => {
       const offset: Coord = [0, i * ITEM_HEIGHT];
-      screen.drawRect(rectTranslate(itemRect, offset), UI_BG_COLOR);
-      screen.drawRect(rectTranslate(iconRect, offset), UI_HIGHLIGHT_COLOR);
-      screen.drawSprite(this.beerSprites.getSprite([1, item.spriteIndex]), coordAdd(iconRect.coord, offset));
-      screen.drawText(item.name, coordAdd(nameCoord, offset), { shadowColor: UI_SHADOW_COLOR });
-      screen.drawText(item.description, coordAdd(descriptionCoord, offset), { size: "small" });
-      screen.drawSprite(this.goldSprite, coordAdd(goldCoord, offset));
-      screen.drawText(item.price + "", coordAdd(priceCoord, offset), { align: "right", shadowColor: UI_SHADOW_COLOR });
+      this.drawItem(screen, rectTranslate(itemRect, offset), item);
     });
+  }
+
+  private drawItem(screen: PixelScreen, rect: Rect, item: Beer) {
+    const iconRect: Rect = { coord: coordAdd(rect.coord, [2, 2]), size: [16, 16] };
+    const goldCoord: Coord = coordAdd(rect.coord, [rect.size[0] - 10, 2]);
+
+    screen.drawRect(rect, UI_BG_COLOR);
+    screen.drawRect(iconRect, UI_HIGHLIGHT_COLOR);
+    screen.drawSprite(this.beerSprites.getSprite([1, item.spriteIndex]), iconRect.coord);
+    screen.drawText(item.name, coordAdd(iconRect.coord, [18, -1]), { shadowColor: UI_SHADOW_COLOR });
+    screen.drawText(item.description, coordAdd(iconRect.coord, [18, 9]), { size: "small" });
+    screen.drawSprite(this.goldSprite, goldCoord);
+    screen.drawText(item.price + "", coordAdd(goldCoord, [-2, -1]), { align: "right", shadowColor: UI_SHADOW_COLOR });
   }
 }
