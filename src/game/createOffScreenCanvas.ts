8625bb131ae7ee958432dacbaf7135d12f9bae20
Rene Saarsoo <nene@triin.net>
2021-10-04 18:02:15 +0300

    Turn off font-smoothing on off-screen canvas

--- src/game/createOffScreenCanvas.ts
+++ src/game/createOffScreenCanvas.ts
@@ -4,6 +4,7 @@ export function createOffScreenCanvas([width, height]: Coord): CanvasRenderingCo
   const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
+  canvas.setAttribute("style", "font-smooth: never; -webkit-font-smoothing: none;");
   const ctx = canvas.getContext("2d");
   if (!ctx) {
     throw new Error("Unable to access Canvas context");
