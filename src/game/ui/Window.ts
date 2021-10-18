6e9904318f7082399b3141ad4e12a772e71106e2
Rene Saarsoo <nene@triin.net>
2021-10-18 13:35:04 +0300

    Render close button inside Window

--- src/game/ui/Window.ts
+++ src/game/ui/Window.ts
@@ -1,11 +1,15 @@
+import { noop } from "lodash";
 import { Coord, coordAdd, coordSub, Rect, rectCenter, rectGrow } from "../Coord";
+import { GameEvent } from "../GameEvent";
 import { PixelScreen } from "../PixelScreen";
 import { drawInset, drawUpset, UI_BG_COLOR, UI_SHADOW_COLOR } from "../ui/ui-utils";
+import { Button } from "./Button";
 
 interface WindowCfg {
   headline: Headline;
   coord?: Coord;
   size: Coord;
+  onClose?: () => void;
 }
 
 export interface Headline {
@@ -18,10 +22,18 @@ const TITLE_HEIGHT = 17;
 export class Window {
   private headline: Headline;
   private rect: Rect;
+  private closeButton: Button;
 
-  constructor({ headline, coord, size }: WindowCfg) {
+  constructor({ headline, coord, size, onClose }: WindowCfg) {
     this.headline = headline;
     this.rect = coord ? { coord, size } : rectCenter({ coord: [0, 0], size }, { coord: [0, 0], size: [320, 200] });
+    this.closeButton = new Button({
+      coord: coordAdd(this.rect.coord, [this.rect.size[0] - 10, 2]),
+      spriteName: "close-button",
+      unpressed: [0, 0],
+      pressed: [1, 0],
+      onPress: onClose || noop,
+    });
   }
 
   paint(screen: PixelScreen) {
@@ -29,6 +41,7 @@ export class Window {
     drawUpset(screen, this.rect);
     drawInset(screen, rectGrow(this.contentAreaRect(), [1, 1]));
     this.drawHeadline(screen);
+    this.closeButton.paint(screen);
   }
 
   getRect(): Rect {
@@ -43,6 +56,10 @@ export class Window {
   contentAreaRect(): Rect {
     const { coord, size } = rectGrow(this.rect, [-3, -3]);
     return { coord: coordAdd(coord, [0, TITLE_HEIGHT]), size: coordSub(size, [0, TITLE_HEIGHT]) };
+  }
+
+  handleGameEvent(event: GameEvent) {
+    this.closeButton.handleGameEvent(event);
   }
 }
 
