43bf9bac59cb4925e4413124fe234f5348873281
Rene Saarsoo <nene@triin.net>
2021-10-19 19:40:01 +0300

    Check if colors are correct, but in wrong order.

--- src/game/questions/ColorsQuestion.ts
+++ src/game/questions/ColorsQuestion.ts
@@ -1,4 +1,4 @@
-import { isEqual } from "lodash";
+import { isEqual, sortBy } from "lodash";
 import { FlagColor } from "../orgs/FlagColors";
 import { randomOrganization } from "../orgs/Organization";
 import { ColorsQuestion } from "./Question";
@@ -11,11 +11,17 @@ export function createColorsQuestion(): ColorsQuestion {
     validate: (colors: FlagColor[]) => {
       if (isEqual(colors, org.colors)) {
         return "Õige!\nTubli rebane. Kiidan.";
+      } else if (isEqual(sortColors(colors), sortColors(org.colors))) {
+        return `Õiged värvid, aga vale järjekord.\nÕige on ${colorsString(org.colors)}. Pead veel pingutama.`;
       } else {
         return `Vale!\n${org.name} värvid on ${colorsString(org.colors)}.\nVõta laituseks sisse.`;
       }
     },
   }
+}
+
+function sortColors(colors: FlagColor[]): FlagColor[] {
+  return sortBy(colors, (c) => c.name);
 }
 
 function colorsString(colors: FlagColor[]): string {
