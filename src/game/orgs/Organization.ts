f36977beb3b1c8bc245db6b7dbf4fb4638eb8ce0
Rene Saarsoo <nene@triin.net>
2021-10-19 18:45:39 +0300

    Add slogans to all orgs

--- src/game/orgs/Organization.ts
+++ src/game/orgs/Organization.ts
@@ -5,6 +5,7 @@ export interface Organization {
   name: string;
   colors: FlagColor[];
   establishedYear: number;
+  slogan?: string;
 }
 
 const orgDef = (org: Organization) => org;
@@ -14,81 +15,97 @@ const orgs = {
     name: "Vironia",
     colors: makeFlag("violett", "must", "valge"),
     establishedYear: 1900,
+    slogan: "Ühisus, kindlus, ausus",
   }),
   estica: orgDef({
     name: "Fraternitas Estica",
     colors: makeFlag("sinine", "roheline", "valge"),
     establishedYear: 1907,
+    slogan: "Teos õiglane ja vahvas, ustav sulle, Eesti rahvas",
   }),
   sakala: orgDef({
     name: "Sakala",
     colors: makeFlag("sinine", "violett", "valge"),
     establishedYear: 1909,
+    slogan: "Üks kõigi eest, kõik ühe eest",
   }),
   ugala: orgDef({
     name: "Ugala",
     colors: makeFlag("must", "sinine", "valge"),
     establishedYear: 1913,
+    slogan: "Isamaa, sõprus, ausus",
   }),
   rotalia: orgDef({
     name: "Rotalia",
     colors: makeFlag("sinine", "must", "roheline"),
     establishedYear: 1913,
+    slogan: "Eestimaa, mu isamaa",
   }),
   liviensis: orgDef({
     name: "Fraternitas Liviensis",
     colors: makeFlag("violett", "roheline", "valge"),
     establishedYear: 1918,
+    slogan: "Vendlus, ausus, hoolsus",
   }),
   leola: orgDef({
     name: "Leola",
     colors: makeFlag("violett", "kollane", "valge"),
     establishedYear: 1920,
+    slogan: "Age quod agis",
   }),
   revelia: orgDef({
     name: "Revelia",
     colors: makeFlag("roheline", "must", "valge"),
     establishedYear: 1920,
+    slogan: "Üks kõige, kõik ühe eest",
   }),
   tehnola: orgDef({
     name: "Tehnola",
     colors: makeFlag("must", "roheline", "valge"),
     establishedYear: 1921,
+    slogan: "Kindlus, ausus, vendlus",
   }),
   tartuensis: orgDef({
     name: "Fraternitas Tartuensis",
     colors: makeFlag("roheline", "valge", "violett"),
     establishedYear: 1929,
+    slogan: "Pidevas töös tõe valguses elurõõmsalt tulevikku",
   }),
   arminia: orgDef({
     name: "Arminia Dorpatensis",
     colors: makeFlag("must", "valge", "kuldne"),
     establishedYear: 1994,
+    slogan: "Jumal, vabadus, isamaa",
   }),
   filiaePatriae: orgDef({
     name: "Filiae Patriae",
     colors: makeFlag("valge", "punane", "roheline"),
     establishedYear: 1920,
+    slogan: "Eesti tütar, Eesti kodu, tugevasti seotud olgu",
   }),
   indla: orgDef({
     name: "Indla",
     colors: makeFlag("kirsipruun", "valge", "roheline"),
     establishedYear: 1924,
+    slogan: "Sõnass kindlust, teosse ausust, kodule armastust",
   }),
   lembela: orgDef({
     name: "Lembela",
     colors: makeFlag("helebeez", "roheline", "violett"),
     establishedYear: 1924,
+    slogan: "Constantia ad finem ducit",
   }),
   amicitia: orgDef({
     name: "Amicitia",
     colors: makeFlag("lilla", "roheline", "kuldne"),
     establishedYear: 1924,
+    slogan: "Per aspera ad astra",
   }),
   sororitasEstoniae: orgDef({
     name: "Sororitas Estoniae",
     colors: makeFlag("valge", "roosa", "must"),
     establishedYear: 2011,
+    slogan: "Teadmised on ilu",
   }),
 };
 
