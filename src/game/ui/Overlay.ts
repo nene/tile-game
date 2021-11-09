dd4e4049b2401481f4d8c2fe9d454fb56b2a2b3e
Rene Saarsoo <nene@triin.net>
2021-11-09 14:09:38 +0200

    Parameterize Overlay opacity

--- src/game/ui/Overlay.ts
+++ src/game/ui/Overlay.ts
@@ -1,7 +1,7 @@
 import { PixelScreen } from "../PixelScreen";
 
 export class Overlay {
-  public static paint(screen: PixelScreen) {
-    screen.drawRect({ coord: [0, 0], size: [320, 200] }, "rgba(0,0,0,0.5)");
+  public static paint(screen: PixelScreen, opacity = 0.5) {
+    screen.drawRect({ coord: [0, 0], size: [320, 200] }, `rgba(0,0,0,${opacity})`);
   }
 }
