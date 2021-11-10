b1ac3b4a0d17f1e4954826feb4abed7593ed3a1b
Rene Saarsoo <nene@triin.net>
2021-11-10 20:02:16 +0200

    Adjust drunkenness depending on level and alcohol

--- src/game/attributes/AlcoSkill.ts
+++ src/game/attributes/AlcoSkill.ts
@@ -3,6 +3,20 @@ import { constrain } from "../utils/constrain";
 
 const drunkennessMinMax = { min: 0, max: 5 };
 
+const levelMultiplier: Record<number, number> = {
+  0: 5, // max: 1 pilku
+  1: 3, // max: 2 pilkut või sass
+  2: 2,
+  3: 1.5,
+  4: 1.25,
+  5: 1,
+  6: 0.75,
+  7: 0.5,
+  8: 0.25,
+  9: 0.2,
+  10: 0.1,
+};
+
 export class AlcoSkill {
   private level = 0; // 0 .. 10
   private drunkenness = 0; // 0 .. 5
@@ -10,19 +24,19 @@ export class AlcoSkill {
   // Consumes 1/4 of a glass (one step from beer glass)
   sip(drink: Drink) {
     if (drink.alcohol > 0) {
-      this.drunkenness = constrain(this.drunkenness + 0.25, drunkennessMinMax);
-      if (this.drunkenness >= drunkennessMinMax.max) {
-        this.levelUp();
-      }
+      this.drunkenness = constrain(this.drunkenness + this.drunkennessPerSip(drink), drunkennessMinMax);
     } else {
       this.drunkenness = constrain(this.drunkenness - 0.25, drunkennessMinMax);
     }
   }
 
+  private drunkennessPerSip(drink: Drink) {
+    return drink.alcohol * levelMultiplier[this.level] * 0.25;
+  }
+
   private levelUp() {
     if (this.level < 10) {
       this.level++;
-      this.drunkenness = 0;
     }
   }
 
@@ -40,6 +54,9 @@ export class AlcoSkill {
   }
 
   reset() {
+    if (this.drunkenness >= drunkennessMinMax.max) {
+      this.levelUp();
+    }
     this.drunkenness = 0;
   }
 }
