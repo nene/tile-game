5e26f6c65ebd8476ec131fa3bdff4f292d158b6e
Rene Saarsoo <nene@triin.net>
2021-11-17 17:52:54 +0200

    Move writing to book decision out of WriteToBookActivity

--- src/game/activities/WriteToBookActivity.ts
+++ src/game/activities/WriteToBookActivity.ts
@@ -12,11 +12,6 @@ export class WriteToBookActivity implements Activity {
   }
 
   tick(figure: CharacterFigure, location: Location): ActivityUpdates {
-    if (!this.character.isRememberingBookWriting()) {
-      this.finished = true;
-      return {};
-    }
-
     const book = this.findBook(location);
     if (book) {
       book.addEntry(this.character);
--- src/game/npc/createCharacterActivities.ts
+++ src/game/npc/createCharacterActivities.ts
@@ -9,12 +9,13 @@ import { MoveActivity } from "../activities/MoveActivity";
 import { tileToScreenCoord } from "../Coord";
 import { WriteToBookActivity } from "../activities/WriteToBookActivity";
 import { Character } from "./Character";
+import { compact } from "lodash";
 
 export function createCharacterActivities(character: Character): Activity[] {
-  return [
+  return compact([
     new MoveToDoorActivity(character),
     new EnterDoorActivity(character),
-    new WriteToBookActivity(character),
+    character.isRememberingBookWriting() ? new WriteToBookActivity(character) : undefined,
     new PauseActivity(5, character),
     new MoveToTableActivity(character),
     new SatisfyDesiresActivity(character),
@@ -22,5 +23,5 @@ export function createCharacterActivities(character: Character): Activity[] {
     new EnterDoorActivity(character),
     new MoveActivity(tileToScreenCoord([10, 15]), character),
     new DespawnActivity(character),
-  ];
+  ]);
 }
