04efffe1a6d6f832457b64167d3d0aa0d563afb9
Rene Saarsoo <nene@triin.net>
2022-01-03 18:40:44 +0200

    Display shop dialog from SellAlcoholActivity

--- src/game/activities/SellAlcoholActivity.ts
+++ src/game/activities/SellAlcoholActivity.ts
@@ -1,10 +1,28 @@
 import { Activity, ActivityUpdates } from "./Activity";
 import { UiController } from "../UiController";
-import { GameItem } from "../items/GameItem";
 import { FeenoksLadyCharacter } from "../npc/FeenoksLadyCharacter";
 import { showPlainTextDialog } from "../dialogs/showPlainTextDialog";
+import { BeerBottle } from "../items/BeerBottle";
+import { getDrink } from "../items/Drink";
+import { Shop } from "../inventory/Shop";
+import { SimpleBottleOpener } from "../items/SimpleBottleOpener";
+import { ShopView } from "../inventory/ShopView";
 
 export class SellAlcoholActivity implements Activity {
+  private shop = new Shop([
+    new BeerBottle(getDrink("limonaad")),
+    new BeerBottle(getDrink("kriek")),
+    new BeerBottle(getDrink("pilsner")),
+    new BeerBottle(getDrink("heineken")),
+    new BeerBottle(getDrink("tommu-hiid")),
+    new BeerBottle(getDrink("alexander")),
+    new BeerBottle(getDrink("special")),
+    new BeerBottle(getDrink("paulaner")),
+    new BeerBottle(getDrink("porter")),
+    new BeerBottle(getDrink("bock")),
+    new SimpleBottleOpener(),
+  ]);
+
   constructor(private character: FeenoksLadyCharacter) {
   }
 
@@ -20,12 +38,25 @@ export class SellAlcoholActivity implements Activity {
     return true;
   }
 
-  interact(ui: UiController, item?: GameItem) {
+  interact(ui: UiController) {
     showPlainTextDialog({
       ui,
       character: this.character,
       text: "Teretulemast Feenoksi alkoparadiisi!",
+      onClose: () => this.showShop(ui),
     });
+  }
+
+  private showShop(ui: UiController) {
+    ui.showInventory(new ShopView({
+      shop: this.shop,
+      wallet: ui.getAttributes().wallet,
+      headline: {
+        title: "Feenoks",
+        description: "Kui seda siin ei müüda, siis polegi mõtet seda osta.",
+      },
+      onClose: () => ui.hideInventory(),
+    }));
   }
 
   nextActivity() {
