bdb2f4f8ec7ca3ca3454d593b8024c7ead31b6d3
Rene Saarsoo <nene@triin.net>
2022-02-08 09:29:39 +0200

    Rename water to ocean :)

--- src/game/items/ItemSpriteLibrary.ts
+++ src/game/items/ItemSpriteLibrary.ts
@@ -4,7 +4,7 @@ import { SpriteLibrary } from "../sprites/SpriteLibrary";
 
 const framesMap = readFramesMap(itemsJson);
 
-export type ItemSpriteType = 'bottle-opener-simple' | 'bottle-opener-locked' | 'tap' | 'drain' | 'book' | 'water';
+export type ItemSpriteType = 'bottle-opener-simple' | 'bottle-opener-locked' | 'tap' | 'drain' | 'book' | 'ocean';
 
 export class ItemSpriteLibrary {
   public static getSprite(name: ItemSpriteType) {
