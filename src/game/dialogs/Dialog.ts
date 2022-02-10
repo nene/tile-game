8187de545a70295051d2c86df611f9999fe8eb30
Rene Saarsoo <nene@triin.net>
2022-02-10 19:05:45 +0200

    Implement Dialog without Window

--- src/game/dialogs/Dialog.ts
+++ src/game/dialogs/Dialog.ts
@@ -1,14 +1,15 @@
-import { Alignment, coordAdd, isCoordInRect, Rect, rectGrow } from "../Coord";
+import { Alignment, coordAdd, coordSub, isCoordInRect, Rect, rectAlign, rectGrow } from "../Coord";
 import { GameEvent } from "../GameEvent";
 import { ColorBandTouch } from "../items/ColorBandTouch";
 import { AcademicCharacter, isAcademicCharacter } from "../npc/AcademicCharacter";
 import { Character } from "../npc/Character";
 import { PixelScreen } from "../PixelScreen";
 import { SpriteLibrary } from "../sprites/SpriteLibrary";
+import { Button } from "../ui/Button";
 import { Component } from "../ui/Component";
 import { DecoratedFrame } from "../ui/DecoratedFrame";
-import { UI_HIGHLIGHT_COLOR } from "../ui/ui-utils";
-import { Window } from "../ui/Window";
+import { SCREEN_RECT } from "../ui/screen-size";
+import { drawInset, UI_BG_COLOR, UI_HIGHLIGHT_COLOR, UI_SHADOW_COLOR } from "../ui/ui-utils";
 import { UiController } from "../UiController";
 
 interface DialogConfig {
@@ -19,40 +20,61 @@ interface DialogConfig {
   align?: Alignment;
 }
 
+const HEADLINE_HEIGHT = 20;
+
 export class Dialog implements Component {
   private ui: UiController;
-  private window: Window;
   private content: Component;
   private onClose?: () => void;
   private character: Character;
   private iconRect: Rect;
   private iconHovered = false;
   private frame: DecoratedFrame;
+  private rect: Rect;
+  private closeButton?: Button;
 
   constructor({ ui, character, createContent, onClose, align }: DialogConfig) {
     this.ui = ui;
     this.onClose = onClose;
     this.character = character;
-    this.window = new Window({
-      headline: {
-        title: character.getName() + ":",
-        description: " ",
-      },
-      headlinePadding: 20,
-      size: [208, 112],
-      align: align ?? "bottom",
-      onClose: onClose
-    });
-    this.content = createContent(rectGrow(this.window.contentAreaRect(), [-2, -2]));
-    this.iconRect = { coord: coordAdd(this.window.getRect().coord, [2, 2]), size: [16, 16] };
-    this.frame = new DecoratedFrame(rectGrow(this.window.getRect(), [8, 8]));
+    this.rect = rectAlign({ coord: [0, 0], size: [208, 112] }, SCREEN_RECT, align || "bottom");
+    this.content = createContent(rectGrow(this.contentAreaRect(), [-2, -2]));
+    this.iconRect = { coord: coordAdd(this.rect.coord, [5, 5]), size: [16, 16] };
+    this.frame = new DecoratedFrame(rectGrow(this.rect, [8, 8]));
+
+    if (onClose) {
+      this.closeButton = new Button({
+        coord: coordAdd(this.rect.coord, [this.rect.size[0] - 13, 6]),
+        spriteName: "close-button",
+        unpressed: [0, 0],
+        pressed: [1, 0],
+        onClick: onClose,
+      });
+    }
+  }
+
+  private contentAreaRect(): Rect {
+    const { coord, size } = rectGrow(this.rect, [-4, -4]);
+    return { coord: coordAdd(coord, [0, HEADLINE_HEIGHT]), size: coordSub(size, [0, HEADLINE_HEIGHT]) };
   }
 
   paint(screen: PixelScreen) {
-    this.window.paint(screen);
-    this.frame.paint(screen);
+    this.drawBackground(screen);
+    this.drawTitle(screen);
     this.drawIcon(screen);
+    this.frame.paint(screen);
+    this.closeButton?.paint(screen);
     this.content.paint(screen);
+  }
+
+  private drawBackground(screen: PixelScreen) {
+    screen.drawRect(this.rect, UI_BG_COLOR);
+    drawInset(screen, rectGrow(this.contentAreaRect(), [2, 1]));
+  }
+
+  private drawTitle(screen: PixelScreen) {
+    const titleCoord = coordAdd(this.iconRect.coord, [this.iconRect.size[0] + 2, 4]);
+    screen.drawText(this.character.getName(), titleCoord, { shadowColor: UI_SHADOW_COLOR });
   }
 
   private drawIcon(screen: PixelScreen) {
@@ -73,11 +95,11 @@ export class Dialog implements Component {
   }
 
   getRect(): Rect {
-    return this.window.getRect();
+    return this.rect;
   }
 
   handleGameEvent(event: GameEvent): boolean | undefined {
-    if (this.window.handleGameEvent(event) || this.content.handleGameEvent(event)) {
+    if (this.closeButton?.handleGameEvent(event) || this.content.handleGameEvent(event)) {
       return true;
     }
 
